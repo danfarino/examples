f3981dafd9b1ed9f20045bd7217c793f89f82d93
Dan Farino <dan.farino@gmail.com>
2023-12-04 15:16:15 -0800

    auto update readme benchmarks

--- advent-of-code-2023/day1/run.sh
+++ advent-of-code-2023/day1/run.sh
@@ -5,4 +5,10 @@ set -euo pipefail
 script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 cd "$script_dir"
 
-go test -bench=. -benchmem .
+exec 3>&1
+{
+    echo '```'
+    echo '$ ./run.sh'
+    go test -bench=. -benchmem . | tee /dev/fd/3
+    echo '```'
+} > README.md
