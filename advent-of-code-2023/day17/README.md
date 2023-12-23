b266e28fd6aead6488d7fa65dadb8ce321475f32
Dan Farino <dan.farino@gmail.com>
2023-12-22 17:06:38 -0800

    no pointers or internal index in priority queue

--- advent-of-code-2023/day17/README.md
+++ advent-of-code-2023/day17/README.md
@@ -4,8 +4,8 @@ goos: linux
 goarch: amd64
 pkg: github.com/danfarino/examples/advent-of-code-2023/day17
 cpu: Intel(R) Core(TM) i7-7700HQ CPU @ 2.80GHz
-BenchmarkInputPart2-8   	       1	2709229645 ns/op	310906528 B/op	 3273231 allocs/op
-BenchmarkInputPart1-8   	       1	2801038306 ns/op	310863808 B/op	 3273105 allocs/op
+BenchmarkInputPart2-8   	       1	1642504259 ns/op	521300040 B/op	 6524757 allocs/op
+BenchmarkInputPart1-8   	       1	1720779635 ns/op	521278192 B/op	 6524693 allocs/op
 PASS
-ok  	github.com/danfarino/examples/advent-of-code-2023/day17	9.174s
+ok  	github.com/danfarino/examples/advent-of-code-2023/day17	5.529s
 ```
--- advent-of-code-2023/priorityqueue/heap.go
+++ advent-of-code-2023/priorityqueue/heap.go
@@ -2,20 +2,14 @@ package priorityqueue
 
 // from the example here: https://pkg.go.dev/container/heap
 
-import (
-	"container/heap"
-)
-
 // A pqItem is something we manage in a priority queue.
 type pqItem[T any] struct {
 	value    T   // The value of the item; arbitrary.
 	priority int // The priority of the item in the queue.
-	// The index is needed by update and is maintained by the heap.Interface methods.
-	index int // The index of the item in the heap.
 }
 
 // A pqHeapInterface implements heap.Interface and holds Items.
-type pqHeapInterface[T any] []*pqItem[T]
+type pqHeapInterface[T any] []pqItem[T]
 
 func (pq pqHeapInterface[T]) Len() int { return len(pq) }
 
@@ -26,14 +20,10 @@ func (pq pqHeapInterface[T]) Less(i, j int) bool {
 
 func (pq pqHeapInterface[T]) Swap(i, j int) {
 	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = i
-	pq[j].index = j
 }
 
 func (pq *pqHeapInterface[T]) Push(x any) {
-	n := len(*pq)
-	item := x.(*pqItem[T]) //nolint:forcetypeassert
-	item.index = n
+	item := x.(pqItem[T]) //nolint:forcetypeassert
 	*pq = append(*pq, item)
 }
 
@@ -41,15 +31,7 @@ func (pq *pqHeapInterface[T]) Pop() any {
 	old := *pq
 	n := len(old)
 	item := old[n-1]
-	old[n-1] = nil  // avoid memory leak
-	item.index = -1 // for safety
+	//old[n-1] = *new(pqItem[T]) // avoid memory leak
 	*pq = old[0 : n-1]
 	return item
-}
-
-// update modifies the priority and value of an item in the queue.
-func (pq *pqHeapInterface[T]) update(item *pqItem[T], value T, priority int) {
-	item.value = value
-	item.priority = priority
-	heap.Fix(pq, item.index)
 }
